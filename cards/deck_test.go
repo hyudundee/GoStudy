50a41d260e9546329cd215a3ec84669bec26af72
hyudundee <hyudundee@gmail.com>
2021-03-06 18:37:22 -0800

    test slice and file IO

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 // t is a test handler
 func TestNewDeck(t *testing.T) {
@@ -8,7 +11,30 @@ func TestNewDeck(t *testing.T) {
 
 	if len(d) != 16 {
 		// t.Error("Expect deck length of 20 but got ", len(d))
+		// interpellation or injection syntax
 		t.Errorf("Expect deck length of 20 but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
+	}
 }
-b
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
