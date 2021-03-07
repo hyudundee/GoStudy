1b793af27603de5ba96d939d16da5f826744a91d
hyudundee <hyudundee@gmail.com>
2021-03-06 21:45:31 -0800

    pointers practice added

--- structs/main.go
+++ structs/main.go
@@ -10,7 +10,8 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	// contact   contactInfo
+	contactInfo // field name contactInfo with type contactInfo
 }
 
 func main() {
@@ -30,10 +31,47 @@ func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		// contact: contactInfo{
+		// 	email:   "jim@gmail.com",
+		// 	zipCode: 94000,
+		// },
+		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
 			zipCode: 94000,
 		},
 	}
-	fmt.Printf("%+v", jim)
+	// jimPointer := &jim
+	// jimPointer.updateName("jimmy")
+	// jim.print()
+
+	// shortcut in pointers in go
+	jim.updateName("jimmy")
+	jim.print()
+
+	/*
+		Whenever you pass an integer, float, string, or struct into a function, what does Go do with that argument?
+		it creates a copy of each argument, and those copies are used inside of the function
+
+		value types { int, float, string, bool, structs }
+		reference types { slices, maps, channels, pointers, function }
+
+		even though reference value is copied, it still points to the location where the value is in RAM
+	*/
+	//big gocha with go
+	mySlice := []string{"Hi", "There", "How", "are", "you"}
+	updateSlice(mySlice)
+	fmt.Println(mySlice)
+}
+
+func updateSlice(s []string) {
+	s[0] = "Bye"
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+// go is a pass by value language whenever we pass some value, a copy will be create in RAM
+func (pointerToPerson *person) updateName(newFirstName string) {
+	pointerToPerson.firstName = newFirstName
 }
