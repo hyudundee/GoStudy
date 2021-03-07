522442993e1396ea8f3806a6a27b84ee8fc00aad
hyudundee <hyudundee@gmail.com>
2021-03-06 23:08:42 -0800

    map practice added

---
+++ map/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		// create empty map
+		var colors map[string]string
+		colors := make(map[string]string)
+	*/
+	/*
+		colors := map[string]string{
+			"red":   "#ff0000",
+			"green": "#4bf745",
+		}
+	*/
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+	}
+	colors["white"] = "#ffffff"
+	delete(colors, "white")
+
+	fmt.Println(colors)
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for ", color, "is ", hex)
+	}
+}
